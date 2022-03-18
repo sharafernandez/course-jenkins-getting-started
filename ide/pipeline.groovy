18f867a7a8a35fad07984aae68a94166de342618
Shara Fernandez <sharameriel@gmail.com>
2022-03-18 23:52:24 +0800

    Modified some parameters to suit my config

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/sharafernandez/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
         stage('Build') {
@@ -23,7 +23,7 @@ pipeline {
                         body: "Please go to ${BUILD_URL} and verify the build", 
                         attachLog: true, 
                         compressLog: true, 
-                        to: "test@jenkins",
+                        to: "sharameriel@gmail.com",
                         recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
